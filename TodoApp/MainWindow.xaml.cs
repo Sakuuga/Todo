6ad145e73c3341163f78219d7ebf3dc3f29382c6
Sakuuga <bezkotaneto4@gmail.com>
2023-04-10 15:14:04 +0300

    WpfApp

--- TodoApp/MainWindow.xaml.cs
+++ TodoApp/MainWindow.xaml.cs
@@ -1,8 +1,21 @@
-﻿using System.Collections.Generic;
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
 using System.Collections.ObjectModel;
-using System.DirectoryServices.ActiveDirectory;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
 using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
 
 namespace TodoApp
 {
@@ -12,28 +25,29 @@ namespace TodoApp
     public partial class MainWindow : Window
     {
         ObservableCollection<Task> tasksList = new ObservableCollection<Task>();
+
         public MainWindow()
         {
             InitializeComponent();
 
-            Task t1 = new Task();
-            t1.Name = "Купити молоко";
-            t1.Description = "Копити молоко 3.2%";
-            t1.IsCompleted = false;
+            //Task t1 = new Task();
+            //t1.Name = "Купити молоко";
+            //t1.Description = "Копити молоко 3.2%";
+            //t1.IsCompleted = false;
 
-            Task t2 = new Task();
-            t2.Name = "Вивчити C#";
-            t2.Description = "Вивчити у CyberBionic Systenatics";
-            t2.IsCompleted = false;
+            //Task t2 = new Task();
+            //t2.Name = "Вивчити C#";
+            //t2.Description = "Вивчити у CyberBionic Systenatics";
+            //t2.IsCompleted = false;
 
-            Task t3 = new Task();
-            t3.Name = "Створити резюме";
-            t3.Description = "Не забути зберегти у PDF";
-            t3.IsCompleted = false;
+            //Task t3 = new Task();
+            //t3.Name = "Створити резюме";
+            //t3.Description = "Не забути зберегти у PDF";
+            //t3.IsCompleted = false;
 
-            tasksList.Add(t1);
-            tasksList.Add(t2);
-            tasksList.Add(t3);
+            //tasksList.Add(t1);
+            //tasksList.Add(t2);
+            //tasksList.Add(t3);
 
             ToDoListBox.ItemsSource = tasksList;
             ToDoListBox.DisplayMemberPath = "Name";
@@ -109,6 +123,24 @@ namespace TodoApp
                 }
             }
             ToDoListBox.ItemsSource = filtered;
+        }
+
+        string fileName = "data.bin";
+
+        private void Window_Closed(object sender, EventArgs e)
+        {
+            string json = JsonConvert.SerializeObject(tasksList);
+            File.WriteAllText(fileName, json);
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            if (File.Exists(fileName))
+            {
+                string json = File.ReadAllText(fileName);
+                tasksList = JsonConvert.DeserializeObject<ObservableCollection<Task>>(json);
+                ToDoListBox.ItemsSource = tasksList;
+            }
         }
     }
 }
--- TodoApp/Task.cs
+++ TodoApp/Task.cs
@@ -1,12 +1,16 @@
-﻿namespace TodoApp
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TodoApp
 {
-        public class Task
+    [Serializable]
+    public class Task
     {
         public string Name { get; set; }
-
         public bool IsCompleted { get; set; }
-
         public string Description { get; set; }
-
     }
 }
